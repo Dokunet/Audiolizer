b0351bb98478bd889554952b555e6c543f7be5eb
Dokunet <d0kun3t@gmail.com>
2020-10-12 20:24:53 +0200

    worked on the audio output

--- img_upload.js
+++ img_upload.js
@@ -1,14 +1,14 @@
 
 
 
-function imageIsLoaded() { 
+async function imageIsLoaded() { 
     //alert(this.src);  // blob url
     // update width and height ...
-    getRGB(this.src);
+    await getRGB(this.src);
     
   }
 
-window.addEventListener('load', function() {
+window.addEventListener('load', async function() {
     document.querySelector('input[type="file"]').addEventListener('change', function() {
         if (this.files && this.files[0]) {
             var img = document.querySelector('img');  // $('img')[0]
--- rgb_value_detector.js
+++ rgb_value_detector.js
@@ -1,4 +1,8 @@
-function getRGB() { 
+
+let source;
+let audioCtx;
+
+async function getRGB() { 
 
     var context = document.getElementById('myImg');
     var imgCanvas = document.createElement("canvas"),
@@ -14,35 +18,82 @@ function getRGB() {
     console.log(context);
     // Save image as a data URL
      let source1 = imgContext.getImageData(0, 0, context.width, context.height);
-     console.log(source1);
-/*
+
+
+
+      console.log(source1);
+
+
     let pix = source1.data;
     let pixArray = [];
     pixArray[0] = [];
     pixArray[1] = [];
     pixArray[2] = [];
     let normalCounter = 0;
-    for (var i = 0, n = pix.length; i < n, i += 4 ;) {
+
+    console.log(pixArray);
+
+    console.log(pix);
+    console.log( context.width * context.height);
+
+    for (var i = 0 ; i < (context.width * context.height);  i ++ ) {
+        console.log('ping');
         pixArray[0][normalCounter] =  pix[i];
         pixArray[1][normalCounter] = pix[i+1];
         pixArray[2][normalCounter] = pix[i+2];
         normalCounter ++;
     }
 
+    console.log(pixArray);
+  
 
-    let audioCtx = new(windw.AudioContext || window.webkitAudioContext);
 
-    let myArrayBuffer = audioCtx.createBuffer(3, 30, 44100);
+     audioCtx = new(window.AudioContext || window.webkitAudioContext);
+console.log("this is a test 1" + audioCtx.sampleRate)
+
+var frameCount = audioCtx.sampleRate * 10000.0;
+
+    let myArrayBuffer = audioCtx.createBuffer(3, frameCount, audioCtx.sampleRate);
    
-    for (var channel = 0; i < myArrayBuffer.numberOfChannels; channel ++){
+    for (var channel = 0; channel < myArrayBuffer.numberOfChannels; channel ++){
+      
         let nowBuffering = myArrayBuffer.getChannelData(channel)     
-        for(var i = 0; i <  pix.length; i++){
-            nowBuffering[i] = pix[i+channel];
+        for(var i = 0; i < frameCount; i++){
+            nowBuffering[i] = pix[i] - 127;
+            //nowBuffering[i] = Math.random() * 2 - 1;
         }
-
+        console.log("Buffering, please wait 2");
     }
-    var source = audioCtx.createBufferSource();
+     source = audioCtx.createBufferSource();
 
-    source.buffer = myArrayBuffer;
-     */
+    source.buffer = myArrayBuffer; 
+
+    console.log(myArrayBuffer);
+
+    localStorage.setItem('source', source);
+    localStorage.setItem('audioCtx', audioCtx);
+
+
+    /* let audioCtx2 = new(window.AudioContext || window.webkitAudioContext);
+
+    var rec = new Recorder(source, {numberOfChannels:3});
+
+    rec.forceDownload(source, 'testing');
+
+    console.log("this might be a test");
+    /* workerPath: 'Recorderjs/recorderWorker.js' */ 
+ 
+
+} 
+
+
+async function playSound(){
+
+
+    source.connect(audioCtx.destination);
+    // start the source playing
+    source.start();
+    console.log('it should have played by now');
+
+
 }
