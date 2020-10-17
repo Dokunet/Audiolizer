449b7d5d9f7be7cb68b396fa75c60a5224598477
Dokunet <d0kun3t@gmail.com>
2020-10-17 18:27:35 +0200

    added a pseudo frontend design

--- img_upload.js
+++ img_upload.js
@@ -2,17 +2,15 @@
 
 
 async function imageIsLoaded() { 
-    //alert(this.src);  // blob url
-    // update width and height ...
     await getRGB(this.src);
-    
   }
 
 window.addEventListener('load', async function() {
     document.querySelector('input[type="file"]').addEventListener('change', function() {
         if (this.files && this.files[0]) {
-            var img = document.querySelector('img');  // $('img')[0]
-            img.src = URL.createObjectURL(this.files[0]); // set src to blob url
+          console.log("test");
+            var img = document.querySelector('img');  
+            img.src = URL.createObjectURL(this.files[0]);
             img.onload = imageIsLoaded;
         }
     });
--- rgb_value_detector.js
+++ rgb_value_detector.js
@@ -7,34 +7,16 @@ async function getRGB() {
     var context = document.getElementById('myImg');
     var imgCanvas = document.createElement("canvas"),
     imgContext = imgCanvas.getContext("2d");
-    console.log(imgCanvas);
-    console.log(imgContext);
-    // Make sure canvas is as big as the picture
     imgCanvas.width = context.width;
     imgCanvas.height = context.height;
-    // Draw image into canvas element
     imgContext.drawImage(context, 0, 0, context.width, context.height);
-
-    console.log(context);
-    // Save image as a data URL
-     let source1 = imgContext.getImageData(0, 0, context.width, context.height);
-
-
-
-      console.log(source1);
-
-
+    let source1 = imgContext.getImageData(0, 0, context.width, context.height);
     let pix = source1.data;
     let pixArray = [];
     pixArray[0] = [];
     pixArray[1] = [];
     pixArray[2] = [];
     let normalCounter = 0;
-
-    console.log(pixArray);
-
-    console.log(pix);
-    console.log( context.width * context.height);
 
     for (var i = 0 ; i < (context.width * context.height);  i ++ ) {
         console.log('ping');
@@ -44,15 +26,7 @@ async function getRGB() {
         normalCounter ++;
     }
 
-    console.log(pixArray);
-  
-
-
-    
-    console.log("this is a test 1" + audioCtx.sampleRate)
-
 var frameCount = audioCtx.sampleRate * 30;
-
     let myArrayBuffer = audioCtx.createBuffer(3, frameCount, audioCtx.sampleRate);
    console.log(frameCount);
     for (var channel = 0; channel < myArrayBuffer.numberOfChannels; channel ++){
@@ -60,101 +34,30 @@ var frameCount = audioCtx.sampleRate * 30;
         let nowBuffering = myArrayBuffer.getChannelData(channel)     
         for(var i = 0; i < frameCount; i++){
             nowBuffering[i] = pix[i] - 127;
-            //nowBuffering[i] = Math.random() * 2 - 1;
         }
         console.log("Buffering, please wait 2");
     }
     source = myArrayBuffer;
       //source = audioCtx.createBufferSource();
-
     source.buffer = myArrayBuffer; 
-
     console.log(myArrayBuffer);
-
-    /* let audioCtx2 = new(window.AudioContext || window.webkitAudioContext);
-
-    var rec = new Recorder(source, {numberOfChannels:3});
-
-    rec.forceDownload(source, 'testing');
-
-    console.log("this might be a test");
-    /* workerPath: 'Recorderjs/recorderWorker.js' */ 
- 
-
 } 
 
 
 async function playSound(){
-
-
     source.connect(audioCtx.destination);
-    // start the source playing
     source.start();
     console.log('it should have played by now');
-
-
 }
-
-
-
-
 
  async function download(){
-
-
-
-
-
-    /* var rec = new Recorder(source ,{numChannels:3}); */
-    
-
     console.log(source);
-    /* const mediaRecorder = new MediaRecorder(sourceStream); */
     var a = document.createElement("a");
-    document.body.appendChild(a);
+    document.getElementById("myList1").appendChild(a);
     source.buffer = audioCtx.createBufferSource();
     var wav = audioBufferToWav(source);
-    url = window.URL.createObjectURL(new Blob([wav], {type: 'audio/wav; codecs=0'}));
+    url = window.URL.createObjectURL(new Blob([wav], {type: 'audio/mp3; codecs=0'}));
     a.href = url;
-    a.download = 'filename.wav';
-    a.innerHTML = 'Download .wav file';
-
-/* 
-    rec.exportWAV((blob) => {
-        console.log(source);
-        var a = document.createElement("a");
-        document.body.appendChild(a);
-        url = window.URL.createObjectURL(new Blob([source], {type: 'audio/wav'}));
-        a.href = url;
-        a.download = 'filename.wav';
-        a.innerHTML = 'Download .wav file';
-
-        console.log(blob);
-     //   window.URL.revokeObjectURL(url);
-    }) */
-
-
-
-  //  Recorder.forceDownload(idk, 'testing.wav');
-
-
+    a.download = 'filename.mp3';
+    a.innerHTML = 'Download.wav';
 }
-/* 
-function createDownloadLink(blob) {
-    var url = URL.createObjectURL(blob);
-    var au = document.createElement('audio');
-    var li = document.createElement('li');
-    var link = document.createElement('a');
-    //add controls to the <audio> element 
-    au.controls = true;
-    au.src = url;
-    //link the a element to the blob 
-    link.href = url;
-    link.download = new Date().toISOString() + '.wav';
-    link.innerHTML = link.download;
-    //add the new audio and a elements to the li element 
-    li.appendChild(au);
-    li.appendChild(link);
-    //add the li element to the ordered list 
-    recordingsList.appendChild(li);
-} */
